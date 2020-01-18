59267e0be168e676739033b341e3c6b74568717a
Luis <takahashihideki408@gmail.com>
2020-01-18 20:18:49 -0300

    Added base/path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,13 @@
 const path = require('path')
 
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    }
+  })
+}
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
