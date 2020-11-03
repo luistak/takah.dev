d931f7d18999eddd5c6c0cd801e6d42fdae38090
Luis <takahashihideki408@gmail.com>
2020-11-02 22:17:49 -0300

    Added initial metaTags and custom metaTags support in template component

--- components/template.tsx
+++ components/template.tsx
@@ -27,9 +27,18 @@ type PageTemplateProps = {
   t: Translate;
   title: string;
   children: ReactNode;
+  meta?: Record<string, string>;
 };
 
-export function PageTemplate({ t, title, children }: PageTemplateProps) {
+const metaTags = {
+  author: 'Luís Takahashi',
+  url:
+    'https://res.cloudinary.com/daiqkausy/image/upload/c_scale,w_200/v1604365911/me.jpg',
+  image_url:
+    'https://res.cloudinary.com/daiqkausy/image/upload/c_scale,w_200/v1604365911/me.jpg',
+};
+
+export function PageTemplate({ t, title, meta, children }: PageTemplateProps) {
   return (
     <ThemeProvider theme={tokens}>
       <Page>
@@ -37,6 +46,34 @@ export function PageTemplate({ t, title, children }: PageTemplateProps) {
         <Head>
           <title>{title}</title>
           <link rel="icon" href="/favicon.png" />
+          <meta name="robots" content="index, follow" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1.0"
+          />
+
+          <meta property="og:type" content="blog" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content={metaTags.url} />
+          <meta property="og:image" content={metaTags.image_url} />
+          <meta property="og:site_name" content={metaTags.author} />
+
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:image" content={metaTags.image_url} />
+          <meta name="twitter:site" content={metaTags.author} />
+          <meta name="twitter:creator" content={metaTags.author} />
+          {meta?.description ? (
+            <>
+              <meta name="twitter:description" content={meta?.description} />
+              <meta property="og:description" content={meta?.description} />
+            </>
+          ) : null}
+
+          {meta
+            ? Object.entries(meta).map(([name, content]) => (
+                <meta key={name} name={name} content={content} />
+              ))
+            : null}
         </Head>
         <Languages />
         <Main>{children}</Main>
