c89e773a41bbf4173bf26e421483e5d30b01398f
Luis Takahashi <takahashihideki408@gmail.com>
2021-02-28 15:30:56 -0300

    Fix

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom';
 
-import { images } from './next.config';
+import * as NextJsConfig from './next.config';
 
 process.env = {
   ...process.env,
   __NEXT_I18N_SUPPORT: 'true',
-  __NEXT_IMAGE_OPTS: (images as unknown) as string,
+  __NEXT_IMAGE_OPTS: (NextJsConfig.images as unknown) as string,
 };
